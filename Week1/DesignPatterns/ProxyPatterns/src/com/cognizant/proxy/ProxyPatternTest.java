aac1be9b3564ec74355a6fcd820109c6e25cac20
Pratyush Kumar <pratyushmohanty12345@gmail.com>
2026-06-18 00:15:40 +0530

    Proxy Patterns Exercise 6

---
+++ Week1/DesignPatterns/ProxyPatterns/src/com/cognizant/proxy/ProxyPatternTest.java
@@ -0,0 +1,19 @@
+package com.cognizant.proxy;
+
+public class ProxyPatternTest {
+
+    public static void main(String[] args) {
+
+        Image image = new ProxyImage("sample.jpg");
+
+        System.out.println("First Display:");
+
+        image.display();
+
+        System.out.println();
+
+        System.out.println("Second Display:");
+
+        image.display();
+    }
+}
