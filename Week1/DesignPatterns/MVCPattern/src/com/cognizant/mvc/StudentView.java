a539c54061a6f1ea4e2cc87c504799438be46e4d
Pratyush Kumar <pratyushmohanty12345@gmail.com>
2026-06-18 00:38:36 +0530

    MVCPattern Exercise 10

---
+++ Week1/DesignPatterns/MVCPattern/src/com/cognizant/mvc/StudentView.java
@@ -0,0 +1,16 @@
+package com.cognizant.mvc;
+
+public class StudentView {
+
+    public void displayStudentDetails(
+            String studentName,
+            String studentId,
+            String studentGrade) {
+
+        System.out.println("Student Details");
+        System.out.println("---------------");
+        System.out.println("Name  : " + studentName);
+        System.out.println("ID    : " + studentId);
+        System.out.println("Grade : " + studentGrade);
+    }
+}
