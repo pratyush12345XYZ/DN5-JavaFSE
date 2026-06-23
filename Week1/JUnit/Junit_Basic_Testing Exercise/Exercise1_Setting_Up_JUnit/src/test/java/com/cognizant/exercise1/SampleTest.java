50946c5d7a2a89a59e2ea7282a71403a604ef550
Pratyush Kumar <pratyushmohanty12345@gmail.com>
2026-06-23 13:36:57 +0530

    Completed JUnit Exercise 1 - Setting Up JUnit

---
+++ Week1/JUnit/Junit_Basic_Testing Exercise/Exercise1_Setting_Up_JUnit/src/test/java/com/cognizant/exercise1/SampleTest.java
@@ -0,0 +1,13 @@
+package com.cognizant.exercise1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SampleTest {
+
+    @Test
+    void testSetup() {
+        assertTrue(true);
+    }
+}
