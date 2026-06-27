4c95cdfa79fa0dd04706802c12fea3f4ad4531b1
Pratyush Kumar <pratyushmohanty12345@gmail.com>
2026-06-28 00:20:35 +0530

    Completed Mockito Exercise 2 - Verifying Interactions

---
+++ Week1/Mockito/Mockito_Exercise/Exercise2_VerifyingInteractions/src/test/java/com/cognizant/mockito/MyServiceTest.java
@@ -0,0 +1,22 @@
+package com.cognizant.mockito;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+import org.junit.jupiter.api.Test;
+
+public class MyServiceTest {
+
+    @Test
+    public void testVerifyInteraction() {
+
+        ExternalApi mockApi = mock(ExternalApi.class);
+
+        MyService service = new MyService(mockApi);
+
+        service.fetchData();
+
+        verify(mockApi).getData();
+    }
+
+}
